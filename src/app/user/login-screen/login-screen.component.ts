bc39e0b88f524cdabf9bf7cebab820e6339efb7c
przemkow <przemkow92@gmail.com>
2017-01-28 15:09:43 +0100

    add loggin screen

--- src/app/user/login-screen/login-screen.component.ts
+++ src/app/user/login-screen/login-screen.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['login-screen.component.scss']
 })
 export class LoginScreenComponent implements OnInit {
+  private nick: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  //TODO rewrite for reactive components.
+  registerUser() {
+    console.log(`add user register here ${this.nick}`);
+  }
+
+  onKey(event:any) {
+    this.nick = event.target.value;
+  }
 }
