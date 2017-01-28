63904c727e9ebde555754d5e7fe32ef0fc16ab6c
Daniel Koza <dakolech@gmail.com>
2017-01-28 15:15:22 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserModule } from './user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -26,7 +27,8 @@ import { LoginScreenComponent } from './user/login-screen/login-screen.component
     StoreModule.provideStore({ counter: counterReducer, board: boardReducer }),
     StoreDevtoolsModule.instrumentOnlyWithExtension({
       maxAge: 5
-    })
+    }),
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
