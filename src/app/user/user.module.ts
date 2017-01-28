91ce83544210c53dd77a15f2f03ba023067d97a4
Damian Bachórz <damian.bachorz@briisk.co>
2017-01-28 13:11:15 +0100

    user effects

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { UserEffects } from './user.effects';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    EffectsModule.run(UserEffects)
   ],
   declarations: [
     UserListComponent,
