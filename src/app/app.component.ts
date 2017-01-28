327087cbc9075310f9a787ac3b2eb6bf41c2c870
Daniel Koza <dakolech@gmail.com>
2017-01-28 20:13:57 +0100

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,9 @@ import { PhoenixChannels } from './helpers';
 })
 export class AppComponent {
   title = 'ecc works!';
-  public socket;
-  public gameChannel;
 
   constructor(
-    phoenixChannels: PhoenixChannels
   ) {
-    this.gameChannel = phoenixChannels.channel('game:lobby');
-    this.gameChannel.join({data: 'asd'}).subscribe(item => console.log(item));
-    const phoenixObservable = this.gameChannel.observeMessage('new_msg');
-    phoenixObservable.subscribe(item => console.log(item));
-  }
 
-  click() {
-    this.gameChannel.send('new_msg',  { user: 'pszemek'} );
   }
 }
