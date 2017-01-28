42673a9b2a9b8585e979bf15757191c4f80eccc9
Daniel Koza <dakolech@gmail.com>
2017-01-28 20:13:18 +0100

    style board"

--- src/app/board/board.reducer.ts
+++ src/app/board/board.reducer.ts
@@ -5,17 +5,20 @@ import { fromJS, Map } from 'immutable';
 
 const initState: Map<string, any> = fromJS({
   player1: {
-    color: 'red',
+    shape: 'circle',
   },
   player2: {
-    color: 'blue',
+    shape: 'cross',
   },
   boxes: Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => ({ playerId: '' }))),
+  currentUser: 'player1',
 });
 
 export function boardReducer(state = initState, action: Action) {
   const actions = {
-    [SET_PAWN]: () => state.setIn(['boxes', action.payload.column, action.payload.row, 'playerId'], state.getIn(['player1', 'color'])),
+    [SET_PAWN]: () => state
+      .setIn(['boxes', action.payload.column, action.payload.row, 'playerId'], state.getIn([state.get('currentUser'), 'shape']))
+      .set('currentUser', state.get('currentUser') === 'player1' ? 'player2' : 'player1'),
     [SET_PLAYER1]: () => state.set('player1', action.payload.player),
     [SET_PLAYER2]: () => state.set('player2', action.payload.player),
   };
